8dddaa5e5a646b0accd19bd32acb61695ecf96da
Wil Faver <wilfa4243@hotmail.com>
2019-08-08 10:30:26 -0500

    Primer commit

---
+++ src/main/java/com/crud/api/CrudSpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.crud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringTutorialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crud/api/model/EmployeeModel.java
@@ -0,0 +1,53 @@
+package com.crud.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+ 
+@Entity
+@Table(name="employees")
+public class EmployeeModel {    
+	
+	@Id
+	private int id;
+	private String name;
+	private String location;
+	
+	public EmployeeModel() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	
+	
+	
+}
